a89f0b875e62562e86ab0968650ae8fa61ca7a01
yd s <song6341@gachon.ac.kr>
2025-07-04 00:03:16 +0900

    Feat: Add padding and one-hot encoding method in encoding.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from nlp_toolkit.preprocessing.tokenizers import Tokenizers
 from nlp_toolkit.preprocessing.encoding import Encoding 
 if __name__ == '__main__':
-    text = "A barber is a person. a barber is good person. a barber is huge person. he Knew A Secret! The Secret He Kept is huge secret. Huge secret. His barber kept his word. a barber kept his word. His barber kept his secret. But keeping and keeping such a huge secret to himself was driving the barber crazy. the barber went up a huge mountain."
-    text = Tokenizers().tokenize_by_sent(text)
-    result = Encoding().int_encoding_by_keras(text)
+    text = "나는 자연어 처리를 배운다"
+    result = Encoding().one_hot_encoding_by_keras(text)
     print(result)
 
--- nlp_toolkit/preprocessing/encoding.py
+++ nlp_toolkit/preprocessing/encoding.py
@@ -4,11 +4,44 @@ from nltk import FreqDist
 from nltk.corpus import stopwords
 from nlp_toolkit.preprocessing.tokenizers import Tokenizers
 from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.utils import to_categorical
+
+
 class Encoding:
     def __init__(self):
         self.stop_words = set(stopwords.words('english'))
         self.word_tokenizer = Tokenizers().tokenize_by_word
         self.tokenizer = Tokenizer(oov_token = 'OOV')
+    
+    def sentence_to_word(self, tokens : list) -> list:
+        """
+        Tokenizes and preprocesses a list of sentences into words.
+
+        Each sentence is tokenized into a list of words.
+        The words are then converted to lowercase, and any stop words or words shorter than three characters are removed.
+
+        Args:
+            tokens(list): A list of sentences (strings) to process
+        
+        Returns:
+            list : A list of lists, where each inner list contains the processed word tokens from a sentence.
+        """
+        preprocessed_sentences = []
+
+        for sentence in tokens:
+            tokenized_sentece_to_word = self.word_tokenizer(sentence)
+            result = []
+
+            for word in tokenized_sentece_to_word:
+                word = word.lower()
+                if word not in self.stop_words:
+                    if len(word) > 2:
+                        result.append(word)
+            preprocessed_sentences.append(result)
+        
+        return preprocessed_sentences
+
     
     def int_encoding_by_dic(self, tokens : list) -> list:
         """
@@ -159,6 +192,108 @@ class Encoding:
         
         self.tokenizer.fit_on_texts(preprocessed_sentences)
         return self.tokenizer.word_index
+    
+    def pad_by_numpy (self, toekns : list) -> np.array:
+        """
+        Encodes and pads sentences into a NumPy array of uniform length.
+
+        This method preprocesses raw text into tokenized sentences, 
+        converts the tokens into integer sequences, and then pads each sequence with zeros to match the length of the longest sentence.
+
+        Args:
+            tokens (list): A list of sentences(string) to encode and pad
+        
+        Returns:
+            np.array: A 2D NumPy array of integer-encoded sentences, with each row padded to a uniform length
+        """
+        preprocessed_sentences = self.sentence_to_word(tokens=toekns)
+        self.tokenizer.fit_on_texts(preprocessed_sentences)
+        encoded = self.tokenizer.texts_to_sequences(preprocessed_sentences)
+
+        max_len = max(len(item) for item in encoded)
+
+        for sentence in encoded:
+            while len(sentence) < max_len:
+                sentence.append(0)
+        
+        padded_np = np.array(encoded)
+
+        return padded_np
+    
+    def pad_by_keras(self, tokens : list) -> np.array:
+        """
+        Encodes and pads sentences using the Keras pad_sequences utility.
+
+        This method preprocesses raw text into tokenized sentences, 
+        converts them to integer sequences, and then applies post-padding to ensure all sequences have a uniform length.
+
+        Args:
+            tokens (list): A list of sentences(string) to encode and pad
+        
+        Returns:
+            np.array: A 2D NumPy array containig the integer-encoded and padded sequences.
+        """
+        preprocessed_sentences = self.sentence_to_word(tokens=tokens)
+        self.tokenizer.fit_on_texts(preprocessed_sentences)
+        encoded = self.tokenizer.texts_to_sequences(preprocessed_sentences)
+
+        padded = pad_sequences(encoded, padding='post')
+
+        return padded
+    
+    def one_hot_encoding(self, tokens : list) -> list:
+        """
+        Performs one-hot encoding on a list of tokens.
+
+        This function creates a unique vocabulary from the input tokens and then generates a corresponding one-hot vector for each token in the list.
+
+        Args:
+            tokens (list): A list of tokens to be one-hot encoded
+
+        Results:
+            list: A list of one-hot encoded vectors, where each vector corresponds to a token in the original list.
+        """
+        result = []
+        word_to_index = {word : index for index, word in enumerate(tokens)}
+        
+        for word in word_to_index:
+            one_hot_vector = [0] * (len(word_to_index))
+            index = word_to_index[word]
+            one_hot_vector[index] = 1
+            result.append(one_hot_vector)
+        
+        return result
+    
+    def one_hot_encoding_by_keras(self, text : str) -> list:
+        """
+        Performs one-hot encoding using Keras to_categorical utility.
+
+        Args:
+            text (str): A String to be encoded.
+
+        Results:
+            list : A list of one-hot encoded vectors where each vector corresponds to a token in the original list.
+        """
+        self.tokenizer.fit_on_texts([text])
+        encoded = self.tokenizer.texts_to_sequences([text])[0]
+        one_hot = to_categorical(encoded)
+
+        return one_hot
+
+
+
+
+
+
+
+
+    
+    
+
+
+
+
+
 
 
         
